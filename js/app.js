818a015a6f5f513d23d47cc6e2382259771d6a5e
Mario Ito <contato@marioito.com.br>
2022-11-06 17:59:09 -0300

    Home completa

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
-const menu_mobile_icon = document.getElementById("menu-mobile-icon");
+const menu_mobile_open = document.getElementById("menu-mobile-icon");
+const menu_mobile_close = document.getElementById("menu-close-icon");
 const menu_mobile = document.getElementById("menu-mobile");
 
-menu_mobile_icon.addEventListener("click", function (){
+menu_mobile_open.addEventListener("click", function (){
     menu_mobile.classList.add("open");
+});
+
+menu_mobile_close.addEventListener("click", function (){
+    menu_mobile.classList.remove("open");
 });
 
