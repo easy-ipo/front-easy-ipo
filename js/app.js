5c2e01d85a7ba3677a977034dc190a9d00d51312
Mario Ito <contato@marioito.com.br>
2022-11-06 17:21:31 -0300

    [WIP] Menu mobile

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const menu_mobile_icon = document.getElementById("menu-mobile-icon");
+const menu_mobile = document.getElementById("menu-mobile");
+
+menu_mobile_icon.addEventListener("click", function (){
+    menu_mobile.classList.add("open");
+});
+
