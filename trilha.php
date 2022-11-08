320ae799dec9dba89a6b561eab4615bc92f8db83
Mario Ito <contato@marioito.com.br>
2022-11-07 23:02:35 -0300

    includes php

---
+++ trilha.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="pt_BR">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Easy IPO - Trilha IPO</title>
+
+  <link rel="icon" type="image/x-icon" href="images/favicon.ico">
+
+  <link rel="stylesheet" href="css/trail.css">
+
+</head>
+<body>
+
+<div class="container">
+
+  <?php include "includes/header.php" ?>
+
+  <div class="content-trail">
+    <div class="wrapper">
+
+      <div class="trail-step">
+        <div class="trail-pin">
+        <span>4</span>
+        </div>
+      </div>
+
+      <div class="trail-step">
+        <div class="trail-pin">
+        <span>3</span>
+        </div>
+      </div>
+
+      <div class="trail-step">
+        <div class="trail-pin">
+        <span>2</span>
+        </div>
+      </div>
+
+      <div class="trail-step">
+        <div class="trail-pin user-pin">
+        <span>1</span>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+    <?php include "includes/footer.php" ?>
+
+  <script src="js/app.js" type="text/javascript"></script>
+
+</div>
+
+</body>
+</html>
