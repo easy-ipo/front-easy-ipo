c3a1bdb69403acb7bed88310ef53d2271e9b858c
Mario Ito <contato@marioito.com.br>
2022-11-10 20:38:33 -0300

    Retirando PHP

--- js/app.js
+++ js/app.js
@@ -1,14 +1,37 @@
-const menu_mobile_open = document.getElementById("menu-mobile-icon");
-const menu_mobile_close = document.getElementById("menu-close-icon");
-const menu_mobile = document.getElementById("menu-mobile");
+// Carregando Header e Footer
+window.addEventListener('load', function () {
+    fetch('includes/header.html')
+        .then((response) => {
+            return response.text();
+        })
+        .then((html) => {
+            // console.log(html)
+            document.getElementById("header-container").innerHTML = html;
+            document.getElementById("footer-container").innerHTML = html;
 
-// Exibindo o menu mobile após o carregamento da página
-menu_mobile.style.display = "flex";
+            // Tirando Preloader
+            document.getElementsByClassName("container")[0].style.display = "block";
+            document.getElementById("preloader").remove();
 
-menu_mobile_open.addEventListener("click", function (){
-    menu_mobile.classList.add("open");
-});
+            // Exibindo o menu mobile após o carregamento da página
+            loadMenuMobile();
+        });
+})
 
-menu_mobile_close.addEventListener("click", function (){
-    menu_mobile.classList.remove("open");
-});
+
+function loadMenuMobile() {
+
+    const menu_mobile_open = document.getElementById("menu-mobile-icon");
+    const menu_mobile_close = document.getElementById("menu-close-icon");
+    const menu_mobile = document.getElementById("menu-mobile");
+
+    menu_mobile.style.display = "flex";
+
+    menu_mobile_open.addEventListener("click", function () {
+        menu_mobile.classList.add("open");
+    });
+
+    menu_mobile_close.addEventListener("click", function () {
+        menu_mobile.classList.remove("open");
+    });
+}
