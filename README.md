61029b11c788c674948e4c828552d18fbcfc2463
Mario Ito <contato@marioito.com.br>
2022-11-19 18:14:42 -0300

    Removendo Preloader e includes

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# front-easy-ipo
+# Challenge Sprint 2
+
+## Responsive Web Development
+
+**Professor**: Adriano Kleber Milanez
+
+### Integrantes do Grupo
+
+- RM 97439 - Ricardo Rubio Sito Antunes
+- RM 97012 - Igor Augusto Takeshigue Lemos
+- RM 97282 - Pablo Lage Carral
+- RM 97192 - Joederson Oliveira Pereira
+- RM 96950 - Mário Ito Bocchini
--- js/app.js
+++ js/app.js
@@ -1,43 +1,13 @@
-// Carregando Header e Footer
-window.addEventListener('load', function () {
-    fetch('includes/header.html')
-        .then((response) => {
-            return response.text();
-        })
-        .then((html) => {
-            document.getElementById("header-placeholder").outerHTML = html;
+const menu_mobile_open = document.getElementById("menu-mobile-icon");
+const menu_mobile_close = document.getElementById("menu-close-icon");
+const menu_mobile = document.getElementById("menu-mobile");
 
-            // Tirando Preloader
-            document.getElementsByClassName("container")[0].style.display = "block";
-            document.getElementById("preloader").remove();
+menu_mobile.style.display = "flex";
 
-            // Carregando o menu mobile após o carregamento da página
-            loadMenuMobile();
-        });
+menu_mobile_open.addEventListener("click", function () {
+    menu_mobile.classList.add("open");
+});
 
-    fetch('includes/footer.html')
-        .then((response) => {
-            return response.text();
-        })
-        .then((html) => {
-            document.getElementById("footer-placeholder").outerHTML = html;
-        });
-})
-
-
-function loadMenuMobile() {
-
-    const menu_mobile_open = document.getElementById("menu-mobile-icon");
-    const menu_mobile_close = document.getElementById("menu-close-icon");
-    const menu_mobile = document.getElementById("menu-mobile");
-
-    menu_mobile.style.display = "flex";
-
-    menu_mobile_open.addEventListener("click", function () {
-        menu_mobile.classList.add("open");
-    });
-
-    menu_mobile_close.addEventListener("click", function () {
-        menu_mobile.classList.remove("open");
-    });
-}
+menu_mobile_close.addEventListener("click", function () {
+    menu_mobile.classList.remove("open");
+});
